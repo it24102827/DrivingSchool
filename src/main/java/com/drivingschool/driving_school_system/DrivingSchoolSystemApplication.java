b9f7d43084fd2e19010eccbf31f6b07470562a42
it24102827 <it24102827@my.sliit.lk>
2025-04-14 18:49:48 +0530

    first commit

---
+++ src/main/java/com/drivingschool/driving_school_system/DrivingSchoolSystemApplication.java
@@ -0,0 +1,13 @@
+package com.drivingschool.driving_school_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingSchoolSystemApplication.class, args);
+	}
+
+}
